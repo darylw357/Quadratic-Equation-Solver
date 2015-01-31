d93dbcf580efec331c13ba3913b423a49fcf604c
Exelion <dw14089@my.bristol.ac.uk>
2015-01-31 16:19:05 +0000

    User Input

--- QuadEqSolver.c
+++ QuadEqSolver.c
@@ -6,35 +6,48 @@
 
 int main(int argc, char *argv[])
 {
-	int a = 1;
-	int b = 20;
-	int c = 5;
+	int a = 0;
+	int b = 0;
+	int c = 0;
 	int d = 0;
-	double x = 0;
-	double complex z = 0;
+	double x1 = 0;
+	double x2 = 0;
+	double complex z1 = 0;
+	double complex z2 = 0;
 	
-	printf("a = %d, b = %d, c = %d\n", a, b, c);
+	printf("This program solves quadratic equations.\n");
+	printf("Please enter 3 integers a, b and c to solve.\n");
+	printf("\nRemember to press space after each number and then hit return when you have typed all 3 values in.\n");
+	
+	scanf("%d, %d, %d\n", &a, &b, &c);
+	
+	printf("You have entered the following equation.\n");
+	printf("%dx^2%+dx%+d\n", a, b, c);
 	
 	d = b * b - (4 * a * c);
 	
 	if(d > 0)
 	{
-	x = (-b+sqrt(b * b -(4 * a * c)))/(2 * a);
-	printf("The roots are %.3f and %.3f\n", x, x);	
+	x1 = (-b + sqrt(d)) / (2 * a);
+	x2 = (-b - sqrt(d)) / (2 * a);
+	printf("The roots are %.3f and %.3f\n", x1, x2);	
 	}
 	else if(d < 0)
 	{
-	z = (-b+csqrt(b * b -(4 * a * c)))/(2 * a);	
-	printf("The roots are complex and %.1f+%.1fi\n", creal(z), cimag(z));
+	z1= (-b +csqrt(d)) / (2 * a);
+	z2 = (-b - csqrt(d)) / (2 *a);
+	printf("The roots are complex %.1f%+.1fi and %.1f%+.1fi\n", creal(z1), cimag(z1), creal(z2), cimag(z2));
 	}	
 	else
 	{
-	x = (-b+sqrt(b * b -(4 * a * c)))/(2 * a);
-	printf("The roots are %.3f and %.3f\n", x, x);		
+	x1 = (-b + sqrt(d)) / (2 * a);
+	x2 = (-b - sqrt(d)) / (2 * a);
+		if(x1 == x2)
+		{
+		printf("There is a repeated root and it is %.3f", x1);	
+		}
 	}
-	
 	
 	getch();
 	return 0;
-	
 }
