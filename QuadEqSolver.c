77f4e9c96590e9b785b8e1aba964fcca1c3c2fa7
Exelion <dw14089@my.bristol.ac.uk>
2015-01-31 15:06:49 +0000

    Conditional Statements

--- QuadEqSolver.c
+++ QuadEqSolver.c
@@ -7,14 +7,33 @@
 int main(int argc, char *argv[])
 {
 	int a = 1;
-	int b = 2;
-	int c = 2;
-	double complex x = 0;
+	int b = 20;
+	int c = 5;
+	int d = 0;
+	double x = 0;
+	double complex z = 0;
 	
 	printf("a = %d, b = %d, c = %d\n", a, b, c);
 	
-	x = (-b+csqrt(b * b -(4 * a * c)))/(2 * a);
-	printf("The positive root is %.1f%+.1fi\n", creal(x), cimag(x));
+	d = b * b - (4 * a * c);
+	
+	if(d > 0)
+	{
+	x = (-b+sqrt(b * b -(4 * a * c)))/(2 * a);
+	printf("The roots are %.3f and %.3f\n", x, x);	
+	}
+	else if(d < 0)
+	{
+	z = (-b+csqrt(b * b -(4 * a * c)))/(2 * a);	
+	printf("The roots are complex and %.1f+%.1fi\n", creal(z), cimag(z));
+	}	
+	else
+	{
+	x = (-b+sqrt(b * b -(4 * a * c)))/(2 * a);
+	printf("The roots are %.3f and %.3f\n", x, x);		
+	}
+	
+	
 	getch();
 	return 0;
 	
